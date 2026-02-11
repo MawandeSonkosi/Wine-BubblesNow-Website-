a30aea6d4bc1ca2bd87c5cad6a25c4ffa8218259
MawandeSonkosi <mawande2sonkosi@gmail.com>
2026-02-11 10:03:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const BACKEND_URL = 'https://www.wineandbubblesnow.co.za';
 
 // Dynamic import for node-fetch
