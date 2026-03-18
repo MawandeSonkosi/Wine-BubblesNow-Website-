0566c6895195a39643037a0119b74ca3de01d401
Sive <sive@Sives-MacBook-Air.local>
2026-03-18 11:33:49 +0200

    Trying to fix banners

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '.')));
 // Health check
 app.get('/api/health', (req, res) => {
   res.json({
